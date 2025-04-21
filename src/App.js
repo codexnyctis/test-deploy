a800513c28b2ea229d6488609625f345f6da90da
Xiang Li <30311502+Lx1808@users.noreply.github.com>
2025-04-21 21:25:35 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// In your App.js or wherever your routes are defined
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import ProjectPage from './pages/ProjectPage';
 import DataReleasePage from './pages/DataReleasePage';
@@ -11,19 +10,17 @@ import MSPSRPI2DetailsPage from './pages/MSPSRPI2DetailsPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/test-deploy" element={<Homepage />} />
-        <Route path="/project" element={<ProjectPage />} />
-        <Route path="/data-release" element={<DataReleasePage />} />
-        <Route path="/publications" element={<PublicationsPage />} />
-        <Route path="/restricted-area" element={<RestrictedArea />} />
-        <Route path="/team" element={<TeamPage />} />
-        <Route path="/projects/mspsrpi-details" element={<MSPSRPIDetailsPage />} />
-        <Route path="/projects/mspsrpi2-details" element={<MSPSRPI2DetailsPage />} />
-        {/* Other routes */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/project" element={<ProjectPage />} />
+      <Route path="/data-release" element={<DataReleasePage />} />
+      <Route path="/publications" element={<PublicationsPage />} />
+      <Route path="/restricted-area" element={<RestrictedArea />} />
+      <Route path="/team" element={<TeamPage />} />
+      <Route path="/projects/mspsrpi-details" element={<MSPSRPIDetailsPage />} />
+      <Route path="/projects/mspsrpi2-details" element={<MSPSRPI2DetailsPage />} />
+      {/* Other routes */}
+    </Routes>
   );
 }
 
